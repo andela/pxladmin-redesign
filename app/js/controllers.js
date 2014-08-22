dd63e84dcc4dd0ff63b6f6e8fad79f0920852b62
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2014-08-22 10:44:45 +0100

    Added register functionality to the page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,12 +1,18 @@
 angular.module('pxlAdmin.controllers', []).
 controller('loginController', function ($scope, pxlAdminService) {
+	var isLoggedIn = false;
 	$scope.doLogin = function() {
 		pxlAdminService.login($scope.pxl_username, $scope.pxl_password).success(function(response) {
-			console.log(response);
+			isLoggedIn = true;
 		});
 	}
 	
 }).
-controller('registerController', function ($scope) {
-	
+controller('registerController', function ($scope, pxlAdminService) {
+	var isRegistered = false;
+	$scope.doRegister = function() {
+		pxlAdminService.register($scope.userdetails).success(function(response) {
+			isRegistered = true;
+		});
+	}
 });
