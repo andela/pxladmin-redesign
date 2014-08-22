dd224f6e3f74a6a1553871b3bfe8b253759fd819
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2014-08-22 10:45:24 +0100

    Added register endpoint URL

--- app/js/services.js
+++ app/js/services.js
@@ -2,13 +2,34 @@ angular.module('pxlAdmin.services', []).
 factory('pxlAdminService', function ($http) {
 
 	var pxlAdmin = {};
+	var base_url = 'http://pxladmindev1.herokuapp.com';
 
 	pxlAdmin.login = function(username, password) {
 		return $http({
 			method: 'POST',
 			// Use server side endpoint
-			url: 'http://pxladmindev1.herokuapp.com/api/login',
-			params: {un: username, pw: password}
+			url: base_url + '/api/login',
+			params: { un: username, pw: password }
+		});
+	}
+
+	pxlAdmin.register = function(userdetails) {
+		return $http({
+			method: 'JSONP',
+			url: base_url + '/api/account/usercreate',
+			params: { accountData: {
+				AmountOutstanding: 0,
+				TotalMonthSpend: 0,
+				campaignLimit: 0,
+				companyName: userdetails.companyName,
+				contactEmail: userdetails.contactEmail,
+	  			contactFirst: userdetails.contactFirst,
+	  			contactLast: userdetails.contactLast,
+	  			contactPass: userdetails.contactPass,
+	  			contactPhone: userdetails.contactPhone,
+	  			sfdcId: userdetails.sfdcId,
+	  			userLimit: 1
+			} }
 		});
 	}
 
