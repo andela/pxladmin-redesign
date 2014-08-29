a3c611fb5c3b2cf697e4935328f688f1d1fd5014
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2014-08-29 11:14:55 +0100

    Finished up with addition of new creatives

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -1,3 +1,4 @@
+/*
 'use strict';
 // Declare app level module which depends on filters, and services
 angular.module('pxlAdminDashboard', [
@@ -9,3 +10,34 @@ config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/:id', {templateUrl: 'partials/campaigns.html', controller: 'campaignController'});
 	// $routeProvider.otherwise({redirectTo: '/login'});
 }]);
+*/
+angular.module('pxlAdminDashboard', [
+	'ui.router',
+	'pxlAdminDashboard.controllers',
+	'pxlAdminDashboard.services'
+]).
+config(function($stateProvider, $urlRouterProvider) {
+    
+    // $urlRouterProvider.otherwise('/:id');
+    
+    $stateProvider
+        
+        // CAMPAIGNS PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('campaigns', {
+            url: '/:id',
+	        views: {
+
+	            // the main template will be placed here (relatively named)
+	            '': {
+	            	templateUrl: 'partials/header.html',
+	            	controller: 'userController'
+	            },
+
+	            'campaignView@campaigns': { 
+	                templateUrl: 'partials/campaigns.html',
+	                controller: 'campaignController'
+	            }
+	        }      
+        });
+        
+});
--- app/dashboard/dashboard_controllers.js
+++ app/dashboard/dashboard_controllers.js
@@ -1,8 +1,46 @@
-angular.module('pxlAdminDashboard.controllers', ['nvd3ChartDirectives']).
-controller('userController', function ($scope, $routeParams, $timeout, userService) {
-	
+angular.module('pxlAdminDashboard.controllers', ['nvd3ChartDirectives', 'angularFileUpload']).
+controller('userController', function ($scope, $stateParams, $timeout, $upload, userService) {
+	$scope.imageSelected = false;
+	$scope.files = []
+	userService.getCreatives($stateParams.id).success(function(response) {
+		if(response.length !== 0) {
+			$('#intro_modal').modal('show');
+		}
+	});
+	$scope.doFileUpload = function() {
+		console.log("Start");
+		for (var i = 0; i < $scope.files.length; i++) {
+	      	var file = $scope.files[i];
+	      	$scope.upload = userService.addCreative($stateParams.id, file, $scope.creative_name).progress(function(evt) {
+	        	console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+	      	}).success(function(data, status, headers, config) {
+	        	console.log(data);
+	        	$('#creative_upload_modal').modal('show');
+	      	});
+	      	//.error(...)
+	      	//.then(success, error, progress); 
+	      	// access or attach event listeners to the underlying XMLHttpRequest.
+	      	//.xhr(function(xhr){xhr.upload.addEventListener(...)})
+	    }
+	    /* alternative way of uploading, send the file binary with the file's content-type.
+	       Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed. 
+	       It could also be used to monitor the progress of a normal http post/put request with large data*/
+	    // $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
+	}
+	$scope.onFileSelect = function($files) {
+	    //$files: an array of files selected, each file has name, size, and type.
+	    $scope.files = $files;
+	    $scope.imageSelected = true;
+	    if($files && $files[0]) {
+		    var reader = new FileReader();
+	        reader.onload = function (e) {
+	            $('#uploadImage').attr('src', e.target.result).width(400).height(200);
+	        };
+	        reader.readAsDataURL($files[0]);
+	    }
+	}
 }).
-controller('campaignController', function($scope, $routeParams, $timeout, userService) {
+controller('campaignController', function($scope, $stateParams, $timeout, userService) {
 	// Returns the date as a double digit
 	var return_double_digit = function(number) {    
 	    if(number <= 99)
@@ -18,7 +56,7 @@ controller('campaignController', function($scope, $routeParams, $timeout, userSe
 	    return d.getFullYear() + "-" + return_double_digit(parseInt(d.getMonth()) + 1);
 	}
 
-    // Default Datasets
+    // Default Zero Datasets
 	$scope.clickData = [{
         "key": "Clicks",
         "values": [ [ 1, 0 ], [ 2, 0 ], [ 3, 0 ], [ 4, 0 ], [ 5, 0 ], [ 6, 0 ], [ 7, 0 ], [ 8, 0 ], [ 9, 0 ], [ 10, 0 ], [ 11, 0 ], [ 12, 0 ], [ 13, 0 ], [ 14, 0 ], [ 15, 0 ], [ 16, 0 ], [ 17, 0 ], [ 18, 0 ], [ 19, 0 ], [ 20, 0 ], [ 21, 0 ], [ 22, 0 ], [ 23, 0 ], [ 24, 0 ], [ 25, 0 ], [ 26, 0 ], [ 27, 0 ], [ 28, 0 ], [ 29, 0 ], [ 30, 0 ], [ 31, 0 ] ]
@@ -29,7 +67,7 @@ controller('campaignController', function($scope, $routeParams, $timeout, userSe
     }];
 	
 	$scope.selectedIndex = 0;
-	$scope.id = $routeParams.id;
+	$scope.id = $stateParams.id;
 	
 	$scope.setSelected = function(index) {
 		var campaign = $scope.campaigns[index];
@@ -49,11 +87,11 @@ controller('campaignController', function($scope, $routeParams, $timeout, userSe
 	    		}
 	    	}
 	    }
+	    // Reassign Datasets based on ImpressionHistory array
 		$scope.clickData = [{
 	        "key": "Clicks",
 	        "values": clickSet
 	    }];
-
 	    $scope.impressionData = [{
 	        "key": "Impressions",
 	        "values": impressionSet
@@ -61,7 +99,7 @@ controller('campaignController', function($scope, $routeParams, $timeout, userSe
     
 	}
 
-	userService.getCampaigns($routeParams.id).success(function(response) {
+	userService.getCampaigns($stateParams.id).success(function(response) {
 		$scope.campaigns = response;
 		$scope.setSelected(0);
 	});
